5800598eccc61fd9baeefeab8dae5e04c8ff1c20
JimmyVazz <cjvasquez_98@hotmail.com>
2020-05-06 16:17:23 -0500

    se hicieron pruebas mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, {useState} from 'react';
+import { Text, View, Button } from 'react-native';
+import {Login} from './components/Login'
 export default function App() {
+  const [nombre, setnombre] = useState("")
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+      <Button
+        title="Hola"
+      />
+      <Login
+        name="Jimmy"
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, Button } from 'react-native';
+
+export const Login = (props) => {
+  return (
+    
+      <Button
+        title={props.name}
+      />
+
+   
+  );
+}
