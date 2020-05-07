3467a479d898f7f47505e6f4346e26c6f8a9a16f
erikrtz <erikrtz@gmail.com>
2020-05-07 18:47:32 -0500

    hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  const [name, setName] = useState("Erik")
+  const [frase, setFrase] = useState("")
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {/* <h1> {name} </h1> */}
+      {/* <button onClick={ () => setName("Jaime")}>Culon</button> */}
+      <h2> tu frase es: {frase} </h2>
+      <input onChange={ (e) => setFrase(e.target.value)} /> 
+      </div>
   );
 }
 
