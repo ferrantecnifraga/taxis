d27108767e742c05fb07a0b4ef8ba60fb9bf774d
erikrtz <erikrtz@gmail.com>
2020-05-29 20:55:16 -0500

    acabe lo primero xd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,29 @@ import React from "react";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 
+import authProvider from './authProvider';
+
+import { UserList } from "./users";
+import { PostList, PostEdit, PostCreate } from './post';
+import Dashboard from './Dashboard';
+
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+
 const dataProvider =
   jsonServerProvider("https://jsonplaceholder.typicode.com");
-
 
 
 export const App = () => {
   
     return (
       <div>
-      <Admin dataProvider={dataProvider} >
-        
+      <Admin dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider}>
+        <Resource name="users" list={UserList} icon={UserIcon} />
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
       </Admin>
       </div>
+      
     );
 
 }
---
+++ frontend/src/post.js
@@ -0,0 +1,55 @@
+import * as React from "react";
+import { List, Datagrid, TextField, ReferenceField, EditButton
+    , SimpleForm, ReferenceInput, SelectInput, TextInput,
+    Create, Edit, SimpleList } from 'react-admin';
+import { useMediaQuery } from '@material-ui/core';
+
+    export const PostList = (props) => {
+        const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+        return (
+            <List {...props}>
+                {isSmall ? (
+                    <SimpleList
+                        primaryText={record => record.title}
+                        secondaryText={record => `${record.views} views`}
+                        tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+                    />
+                ) : (
+                    <Datagrid>
+                        <TextField source="id" />
+                        <ReferenceField label="User" source="userId" reference="users">
+                            <TextField source="name" />
+                        </ReferenceField>
+                        <TextField source="title" />
+                        <TextField source="body" />
+                        <EditButton />
+                    </Datagrid>
+                )}
+            </List>
+        );
+    }
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Create>
+);
+
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
++           <TextInput disabled source="id" />
+            <ReferenceInput source="userId" reference="users">
++               <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
++           <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+);
