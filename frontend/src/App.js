b15180b64261d890bd075555f5574c95762feb11
erikrtz <erikrtz@gmail.com>
2020-06-02 19:37:43 -0500

    NavBar terminada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,12 @@ import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from './post';
 import Dashboard from './Dashboard';
 
-import PostIcon from '@material-ui/icons/Book';
-import UserIcon from '@material-ui/icons/Group';
+import AirportShuttleIcon from '@material-ui/icons/AirportShuttle';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import ErrorIcon from '@material-ui/icons/Error';
+import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
 
 const dataProvider =
   jsonServerProvider("https://jsonplaceholder.typicode.com");
@@ -19,9 +23,13 @@ export const App = () => {
   
     return (
       <div>
-      <Admin dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider}>
-        <Resource name="users" list={UserList} icon={UserIcon} />
-        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+      <Admin  dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider}>
+        <Resource name="Viajes" list={UserList} icon={AirportShuttleIcon} />
+        <Resource name="Choferes" list={PostList} edit={PostEdit} create={PostCreate} icon={AssignmentIndIcon} />
+        <Resource name="Licencias" list={UserList} icon={ContactMailIcon} />
+        <Resource name="Facturas" list={UserList} icon={BarChartIcon} />
+        <Resource name="Sanciones" list={UserList} icon={ErrorIcon} />
+        <Resource name="Usuarios" list={UserList} icon={PeopleOutlineIcon} />
       </Admin>
       </div>
       
