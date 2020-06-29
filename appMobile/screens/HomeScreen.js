7d67ce0b75215e1f2fb1c69d4e0cc8fadeef9bf9
JimmyVazz <cjvasquez_98@hotmail.com>
2020-06-29 01:55:56 -0500

    screen home

--- appMobile/screens/HomeScreen.js
+++ appMobile/screens/HomeScreen.js
@@ -1,28 +1,101 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { View, Text, StyleSheet, ProgressBarAndroid,
+ActivityIndicator, FlatList } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 
 const HomeScreen = ({navigation}) => {
+
     return (
+
+      <View>
+        <Card 
+            title='Viajes esta semana'
+            titleStyle={{
+              fontSize: 15,
+              textAlign: 'left',
+              color: '#1976d2'
+            }}
+            containerStyle={{
+              borderLeftColor: '#1976d2',
+              borderLeftWidth: 3
+            }}>
+              <View style={{display: "flex",flexDirection: "row"}}>
+
+                <Text style={{fontSize: 25}} >27</Text>
+                <View style={{flexGrow: 1}} />
+                <Icon name="car" type='material-community' color="#9e9e9e" size= {25} /> 
+              
+              </View>
+          </Card>
+          <Card 
+            title='Total ganado esta semana'
+            titleStyle={{
+              fontSize: 15,
+              textAlign: 'left',
+              color: '#66bb6a'
+            }}
+            containerStyle={{
+              borderLeftColor: '#66bb6a',
+              borderLeftWidth: 3
+            }}>
+              <View style={{display: "flex",flexDirection: "row"}}>
       
-      <Card 
-      title='Viajes está semana'
-      titleStyle={{
-        fontSize: 15,
-        textAlign: 'left',
-        color: '#1976d2'
-      }}
-      containerStyle={{
-        borderLeftColor: '#1976d2',
-        borderLeftWidth: 3
-      }}>
-        <View style={{display: "flex",flexDirection: "row"}}>
-          <Text style={{fontSize: 25}} >27</Text>
-          <View style={{flexGrow: 1}} />
-          <Icon name="car" type='material-community' color="#9e9e9e" size= {25} /> 
-        </View>
-     </Card>
+                <Text style={{fontSize: 25}} >215,000</Text>
+                <View style={{flexGrow: 1}} />
+                <Icon name="euro" type='font-awesome' color="#9e9e9e" size= {25} /> 
+              
+              </View>
+          </Card>
+          <Card 
+            title='Viajes programados'
+            titleStyle={{
+              fontSize: 15,
+              textAlign: 'left',
+              color: '#00bcd4'
+            }}
+            containerStyle={{
+              borderLeftColor: '#00bcd4',
+              borderLeftWidth: 3
+            }}>
+              <View style={{display: "flex",flexDirection: "row"}}>
       
+              <Text style={{fontSize: 25}} >13</Text>
+              <View style={{flexGrow: 1}} />
+                <ProgressBarAndroid
+                  styleAttr="Horizontal"
+                  indeterminate={false}
+                  progress={0.9}
+                  color='#00bcd4'
+                  style={{
+                    width: 150
+                  }}
+                /> 
+                <View style={{flexGrow: 1}} />
+                <Icon name="calendar" type='font-awesome' color="#9e9e9e" size= {25} /> 
+              
+              </View>
+          </Card>
+          <Card 
+            title='Quejas sin atender'
+            titleStyle={{
+              fontSize: 15,
+              textAlign: 'left',
+              color: '#e53935'
+            }}
+            containerStyle={{
+              borderLeftColor: '#e53935',
+              borderLeftWidth: 3
+            }}>
+              <View style={{display: "flex",flexDirection: "row"}}>
+      
+                <Text style={{fontSize: 25}} >2</Text>
+                <View style={{flexGrow: 1}} />
+                <Icon name="exclamation" type='font-awesome' color="#9e9e9e" size= {25} /> 
+              
+              </View>
+          </Card>
+
+       </View>
     );
 };
 
--- appMobile/screens/MensajesScreen.js
+++ appMobile/screens/MensajesScreen.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
+
+const users = [
+  {
+     name: 'avatar1',
+     avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+  },
+  {
+      name: 'avatar2'
+  }
+ ]
 
 const MensajesScreen = () => {
     return (
-      <View style={styles.container}>
-        <Text>MensajesScreen</Text>
-        
+      <View>
+        <Card containerStyle={{padding: 0}} >
+        {
+          users.map((u, i) => {
+            return (
+              <ListItem
+                key={i}
+                roundAvatar
+                title={u.name}
+                avatar={{uri:u.avatar}}
+              />
+            );
+          })
+        }
+        </Card> 
       </View>
     );
 };
