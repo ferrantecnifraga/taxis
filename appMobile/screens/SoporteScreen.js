a28ca55c2a6f7ce31c06d4f57a510c3480fb810a
Erik <erikrtz@gmail.com>
2020-07-12 16:01:59 -0500

    Formulario de soporteScreen

--- appMobile/screens/SoporteScreen.js
+++ appMobile/screens/SoporteScreen.js
@@ -1,11 +1,47 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { View, StyleSheet } from 'react-native';
+import { Card, Text, Divider, Input, Button } from 'react-native-elements';
+import { Picker } from '@react-native-community/picker';
 
 const SoporteScreen = () => {
+
+  const [selectedValue, setSelectedValue] = useState("");
+
     return (
-      <View style={styles.container}>
-        <Text>SoporteScreen</Text>
-        
+      <View>
+        <Card>
+          <Text style={{color:"#777777", textAlign: 'center', fontSize: 20, margin: 30}}>Reporta errores o sugiere un cambio</Text>
+          <Divider/>
+          <Input
+            containerStyle={styles.containerInput}
+            label='Nombre'
+            placeholder='Tu nombre'
+          />
+          <Input
+            containerStyle={styles.containerInput}
+            label='Email'
+            placeholder='TuEmail@ejemplo.com '
+          />
+          <Text style={{ fontSize: 16 , color: '#86939e', fontWeight: 'bold', marginLeft: 10 }}>Tipo: </Text>
+          <Picker
+            selectedValue={selectedValue}
+            style={{ height: 50, width: 150, margin:10 }}
+            onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+          >
+            <Picker.Item label="Error" value="error" />
+            <Picker.Item label="Funcionalidad" value="func" />
+          </Picker>
+          <Input
+            label='Cuentanos que pasá...'
+            placeholder='Cuentanos que pasá...'
+            multiline={true}  
+            numberOfLines={5}  
+          />
+          <Button
+            title="Enviar"
+            buttonStyle={{width: '70%', alignSelf: 'center', marginTop: 30, backgroundColor: '#009387'}}
+          />
+        </Card>
       </View>
     );
 };
@@ -13,9 +49,7 @@ const SoporteScreen = () => {
 export default SoporteScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
-  },
+  containerInput: {
+    marginTop: 10
+  }
 });
