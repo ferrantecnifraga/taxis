84d132ffafdac9323797208f81c9820b52b0f871
ErikRtz <erikrtz@gmail.com>
2020-07-27 14:39:35 -0500

    Incidencias Tabla ST

--- appMobile/screens/IncidenciasScreen.js
+++ appMobile/screens/IncidenciasScreen.js
@@ -1,34 +1,93 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { Table, TableWrapper, Row, Cell } from 'react-native-table-component';
 
-const IncidenciasScreen = () => {
-  const list = [
-    {
-       name: 'Choque 17/05/2020',
-       subtitle: 'Acabo de chocar'
-    },
-    {
-        name: 'Reporte 11/08/2020',
-        subtitle: 'El cliente no se presento y no hay rep...'
-    },
-   ];
-   
-  
+import  {Button} from 'react-native-elements'
 
-  return ( 
-  <View>
-    {
-      list.map((l, i) => (
-        <ListItem
-          key={i}
-          title={l.name}
-          subtitle={l.subtitle}
-          bottomDivider
-        />
-      ))
+import Modal from 'react-native-modal';
+ 
+export default class IncidenciasScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableHead: ['Tipo', 'Estatus', 'Descripción', 'Lugar', 'Taxista', 'ID Viaje', 'Atendida por', 'Acciones'],
+      tableData: [
+        ['1', '2', '3', '4', '1', '2', '3', '4'],
+        ['1', '2', '3', '4', '1', '2', '3', '4'],
+        ['1', '2', '3', '4', '1', '2', '3', '4'],
+      ]
     }
-  </View>
-  )
-};
-export default IncidenciasScreen;
+  }
+ 
+  
+ 
+  state = {
+    isModalVisible: false,
+  };
+ 
+  toggleModal = () => {
+    this.setState({isModalVisible: !this.state.isModalVisible});
+  };
+
+  render() {
+    
+    const state = this.state;
+    const element = (data, index) => (
+      <TouchableOpacity onPress={() => this.toggleModal(index)}>
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>button</Text>
+        </View>
+      </TouchableOpacity>
+    );
+ 
+    return (
+      <ScrollView horizontal={true}>
+        <Table borderStyle={{borderColor: 'transparent'}}>
+          <Row data={state.tableHead} style={styles.head} textStyle={styles.text}/>
+          {
+            state.tableData.map((rowData, index) => (
+              <TableWrapper key={index} style={styles.row}>
+                {
+                  rowData.map((cellData, cellIndex) => (
+                    <Cell key={cellIndex} data={cellIndex === 7 ? element(cellData, index) : cellData} textStyle={styles.text}/>
+                  ))
+                }
+              </TableWrapper>
+            ))
+          }
+        </Table>
+        <Modal isVisible={this.state.isModalVisible}
+        style={{backgroundColor: '#fff',
+          padding: 20,
+          alignSelf: "center",
+          width: 270,
+          marginVertical: 100
+          
+      }}
+        >
+          <View style={{flex: 1}}>
+            <Text>Hello!</Text>
+            <Text>Hello!</Text>
+            <Text>Hello!</Text>
+            <Text>Hello!</Text>
+            <Text>Hello!</Text>
+            <Text>Hello!</Text>
+            
+            <Button
+            buttonStyle={{marginTop: 30, width: '80%', alignSelf: 'center', backgroundColor: '#009387'}}
+             title="Hide modal" onPress={this.toggleModal} />
+          </View>
+        </Modal>
+      </ScrollView>
+    )
+  }
+}
+ 
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  head: { height: 40, backgroundColor: '#808B97' },
+  text: { margin: 6 },
+  row: { flexDirection: 'row', backgroundColor: '#FFF1C1' },
+  btn: { width: 58, height: 18, backgroundColor: '#78B7BB',  borderRadius: 2 },
+  btnText: { textAlign: 'center', color: '#fff' }
+});
