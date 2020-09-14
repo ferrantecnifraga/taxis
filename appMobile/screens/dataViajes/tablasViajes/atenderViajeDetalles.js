eb6523f2a05a85efe8970b2181d1c24a8884bd85
JimmyVazz <cjvasquez_98@hotmail.com>
2020-09-14 17:17:24 -0500

    ya casi

--- appMobile/screens/dataViajes/tablasViajes/atenderViajeDetalles.js
+++ appMobile/screens/dataViajes/tablasViajes/atenderViajeDetalles.js
@@ -1,6 +1,12 @@
 import React from "react";
-import { View, Text, ScrollView, StyleSheet,Image } from "react-native";
+import { View, Text, ScrollView, StyleSheet,Image, Alert } from "react-native";
 import { Card, Divider, Button } from "react-native-elements";
+
+import AsyncStorage from '@react-native-community/async-storage';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import * as Calendar from 'expo-calendar';
+import * as Localization from 'expo-localization';
+import moment from "moment";
 
 const   atenderViajeDetalles = ({route, navigation}) => {
 
@@ -30,12 +36,12 @@ const { idVP, fechaInicio, direccionHospital, servicio } = route.params;
                     type="outline"
                     titleStyle={{color:'#009387'}}
                     buttonStyle={{alignSelf: 'center', marginTop: 30, marginLeft: 10, borderColor:'#009387' }}
-                    onPress={() => navigation.navigate( 'viajeRechazado')}
+                    onPress={atenderElViaje(idVP, "Rechazar", )} //Pasale navigation anda mas
                 />
                 <Button
                     title="Aceptar viaje"
                     buttonStyle={{alignSelf: 'center', marginTop: 30,  marginRight: 10 ,backgroundColor: '#009387'}}
-                    onPress={() => navigation.navigate( 'viajeAtendido' )}
+                    onPress={atenderElViaje(idVP, "Confirmar") } //Pasale todo los ´parametros de la linea 56
                 />
                 </View>
         
@@ -44,6 +50,96 @@ const { idVP, fechaInicio, direccionHospital, servicio } = route.params;
     </ScrollView>
   );
 };
+
+ //Fetch a la API atender viaje
+ const atenderElViaje = async(idVP, respuesta, {navigation}, fechaInicio, direccionHospital, servicio) => {
+  let email2 = await AsyncStorage.getItem('email')
+    let password2 = await AsyncStorage.getItem('password')
+    let idTaxista2 = await AsyncStorage.getItem('idTaxista')
+    let calendario = await AsyncStorage.getItem('idCalendario')
+
+  try {
+    let response = await fetch('https://taxis-lleida.herokuapp.com/api/taxistas/atenderViaje', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email2,
+        password: password2,
+        idTaxista: idTaxista2,
+        idVP: idVP,
+        respuesta: respuesta
+      })
+    })
+
+    let response2 = await response.json()
+
+
+    if(String(response2.tipo) == "Confirmado") {
+
+      //Viaje calendario
+        let fecha = moment(fechaInicio).toDate();
+        console.warn("Fecha: "+fecha)
+        let details = {
+          title: "Viaje asignado: " +servicio,
+          startDate: fecha,
+          endDate: fecha,
+          timeZone: Localization.timezone,
+          endTimeZone: Localization.timezone,
+          location: direccionHospital,
+          notes: "Servicio: "+servicio
+        }
+        
+        const crear = await Calendar.createEventAsync(calendario, details)
+        if (crear != undefined) {
+          navigation.navigate( 'viajeAtendido' )
+        }else{
+          Alert.alert(
+            "Atender viaje",
+            "Confirmaste el viaje pero no pudimos agregarlo al calendario, disculpa :( . Verifica si configuraste el calendario en el menú. ",
+            [
+              {
+                text: "Ok",
+                onPress: () => console.warn("OK"),
+                
+              }
+      
+            ],
+            { cancelable: true }
+          )
+        }
+        
+  
+        
+        //Viaje calendario
+      
+    } else if(String(response2.tipo) == "Rechazado") {
+      
+      navigation.navigate( 'viajeRechazado')
+
+  
+    }
+  } catch (error) {
+    Alert.alert(
+      "Atender viaje",
+      error,
+      [
+        {
+          text: "Ok",
+          onPress: () => console.warn("OK"),
+          
+        }
+
+      ],
+      { cancelable: true }
+    )
+  }
+
+  
+}
+
 
 export default atenderViajeDetalles;
 
