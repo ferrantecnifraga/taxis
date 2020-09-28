3c09f54e9ef5534f5276c1ad5fd840a79e47d3d8
Erik <erikrtz@gmail.com>
2020-09-28 10:08:47 -0500

    Je telecharge les Views de l'enterprise mon brotha

--- appMobile/screens/vigsoft.js
+++ appMobile/screens/vigsoft.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Text, ScrollView, StyleSheet,Image, Button, TouchableOpacity, View } from "react-native";
-import { Card } from "react-native-elements";
+import { Text, ScrollView, StyleSheet,Image, TouchableOpacity, View } from "react-native";
+import { Card, Button } from "react-native-elements";
 import { Linking } from 'react-native'
 
 const vigsoft = ({navigation}) => {
@@ -15,57 +15,54 @@ const vigsoft = ({navigation}) => {
 
       return (
         <ScrollView>
-            <Card>
+          <Card>
             <Image
                 source={  require("../assets/vigLogo.png")}
-                style={{width: 200, height: 100, alignSelf: 'center', marginTop: 30 }}
+                style={{width: 200, height: 100, alignSelf: 'center', marginTop: 30, borderRadius: 10 }}
             />
-                <Button styles={styles.title} onPress={() => Linking.openURL('mailto:ferrantclk@gmail.com?subject=SendMail&body=Description') }
-      title="Envíanos un correo: Ferran Prieto - CEO" />
-      <Button styles={styles.title} onPress={() => Linking.openURL('mailto:ferrantclk@gmail.com?subject=SendMail&body=Description') }
-      title="Envíanos un correo: Jimmy Vasquez - CTO" />
-                <TouchableOpacity
-
-                 onPress={()=>{dialCall(+34647738533)}}
-                >
-                <View style={styles.btn}>
-    <Text style={styles.btnText}>Llámanos: +34647738533</Text>
-      </View>
-                </TouchableOpacity>
-                <Button buttonStyle={{width: '70%', alignSelf: 'center', marginTop: 5, marginBottom: 5, backgroundColor: '#009387'}} onPress={() => Linking.openURL('vigsoft.tech') }
-      title="Checa nuestra web" />
-
-<Button buttonStyle={{width: '70%', alignSelf: 'center', marginTop: 5, marginBottom: 5, backgroundColor: '#009387'}} onPress={() => Linking.openURL('https://www.facebook.com/Vigsoft/') }
-      title="Vísitanos en Facebook" />
-      
-      
-            </Card>
+            <Button styles={styles.title} buttonStyle={styles.btnTitle} onPress={() => Linking.openURL('mailto:ferrantclk@gmail.com?subject=SendMail&body=Description') }
+              title="Envíanos un correo:  Ferran Prieto - CEO" />
+            <Button styles={styles.title} buttonStyle={styles.btnTitle} onPress={() => Linking.openURL('mailto:ferrantclk@gmail.com?subject=SendMail&body=Description') }
+              title="Envíanos un correo: Jimmy Vasquez - CTO" />
+            <TouchableOpacity onPress={()=>{dialCall(+34647738533)}}>
+              <View style={styles.Viewbtn}>
+                  <Text style={styles.btnText}>Llámanos: +34647738533</Text>
+              </View>
+            </TouchableOpacity>
+            <Button buttonStyle={{width: '70%', alignSelf: 'center', marginTop: 5, marginBottom: 5, backgroundColor: '#009387'}} onPress={() => Linking.openURL('vigsoft.tech') }
+              title="Checa nuestra web" />
+            <Button buttonStyle={{width: '70%', alignSelf: 'center', marginTop: 5, marginBottom: 5, backgroundColor: '#009387'}} onPress={() => Linking.openURL('https://www.facebook.com/Vigsoft/') }
+              title="Vísitanos en Facebook" />
+          </Card>
         </ScrollView>
       );
     };
 
     export default vigsoft;
 
-const styles = StyleSheet.create({
-    title:{
-        textAlign:'center',
-        fontSize: 20,
-        marginTop: 10
-    },
-    text: {
-        textAlign: 'auto',
-        marginTop: 10
-    },
-    btn: { 
-        width: 100, 
-        height: 25, 
-        backgroundColor: '#f9a825',  
-        borderRadius: 5,
-        padding:1,
-        alignSelf: 'center',
+
+    const styles = StyleSheet.create({
+      title:{
+          textAlign:'center',
+          fontSize: 20,
+          marginTop: 10
       },
-      btnText: { 
-        textAlign: 'center', 
-        color: '#fff' 
+      btnTitle: {
+          width: '100%',
+          marginTop: 20,
+          alignSelf: 'center'
       },
-});
+      Viewbtn: { 
+          width: 150, 
+          height: 40, 
+          marginVertical:10,
+          backgroundColor: '#f9a825',  
+          borderRadius: 5,
+          padding:1,
+          alignSelf: 'center',
+        },
+        btnText: { 
+          textAlign: 'center', 
+          color: '#fff' 
+        },
+  });
