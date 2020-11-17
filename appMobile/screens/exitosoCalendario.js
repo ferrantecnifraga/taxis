310151ad87fe42b0de9fe17402296592f43bb7d4
JimmyVazz <cjvasquez_98@hotmail.com>
2020-11-17 09:11:52 -0600

    se arreglo navigation

--- appMobile/screens/exitosoCalendario.js
+++ appMobile/screens/exitosoCalendario.js
@@ -1,70 +1,71 @@
 import React from "react";
 import { View, Text, ScrollView, StyleSheet, Image } from "react-native";
-import { Card,  Button, Icon } from "react-native-elements";
-import * as Animatable from 'react-native-animatable';
+import { Card, Button, Icon } from "react-native-elements";
+import * as Animatable from "react-native-animatable";
+import { useNavigation } from "@react-navigation/native";
 
-const exitosoCalendario = ({navigation}) => {
-
+const exitosoCalendario = ({ navigation }) => {
   return (
-      <View>
-          <Card containerStyle={styles.cardContainer} >
-            <Animatable.View
-            animation="bounceIn"
-            duration={1500}>
-                <Icon
-                name='check-circle'
-                type='material-community'
-                color='#4caf50'
-                size={70}
-                iconStyle={styles.icon}
-                />
-            </Animatable.View>
-              <Text style={styles.titleMsg} >
-                ¡Gracias!
-              </Text>
-              <Text style={styles.textMsg}>
-                  Todos tus viajes se sincronizaran con tu calendario
-              </Text>
-              <Button
-                icon={
-                    <Icon
-                    name="check-circle"
-                    type='material-community'
-                    size={25}
-                    color="white"
-                    iconStyle={{marginRight:10}}
-                    />
-                }
-                title="Ir a inicio"
-                buttonStyle={{width: '70%', alignSelf: 'center', backgroundColor: '#009387', marginBottom: 20}}
-                onPress={() => navigation.navigate('Home', {screen: 'Home'} )}
-                />
-          </Card>
-      </View>
+    <View>
+      <Card containerStyle={styles.cardContainer}>
+        <Animatable.View animation="bounceIn" duration={1500}>
+          <Icon
+            name="check-circle"
+            type="material-community"
+            color="#4caf50"
+            size={70}
+            iconStyle={styles.icon}
+          />
+        </Animatable.View>
+        <Text style={styles.titleMsg}>¡Gracias!</Text>
+        <Text style={styles.textMsg}>
+          Todos tus viajes se sincronizaran con tu calendario
+        </Text>
+        <Button
+          icon={
+            <Icon
+              name="check-circle"
+              type="material-community"
+              size={25}
+              color="white"
+              iconStyle={{ marginRight: 10 }}
+            />
+          }
+          title="Ir a inicio"
+          buttonStyle={{
+            width: "70%",
+            alignSelf: "center",
+            backgroundColor: "#009387",
+            marginBottom: 20,
+          }}
+          onPress={() => navigation.navigate("Home", { screen: "Home" })}
+        />
+      </Card>
+    </View>
   );
 };
 
 export default exitosoCalendario;
 
 const styles = StyleSheet.create({
-    titleMsg: {
-        alignSelf: 'center',
-        fontSize: 20,
-        textAlign: 'center',
-        marginTop: 60,
-        color: '#424242'
-    },
-    textMsg: {
-        alignSelf: 'center',
-        fontSize: 20,
-        textAlign: 'center',
-        marginBottom: 50,
-        color: '#424242'
-    },
-    icon:{
-        marginTop: 60
-    },
-    cardContainer: {
-        marginVertical: '10%',
-    }
+  titleMsg: {
+    alignSelf: "center",
+    fontSize: 20,
+    textAlign: "center",
+    marginTop: 60,
+    color: "#424242",
+  },
+  textMsg: {
+    alignSelf: "center",
+    fontSize: 20,
+    textAlign: "center",
+    marginBottom: 50,
+    color: "#424242",
+  },
+  icon: {
+    marginTop: 60,
+  },
+  cardContainer: {
+    marginVertical: "10%",
+  },
 });
